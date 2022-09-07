0163030d79686a965fd79273be77b217d1ee274d
henry <igwehenry740@gmail.com>
2022-09-07 23:29:58 +0100

    next box and added questions no in script.js

--- script.js
+++ script.js
@@ -20,15 +20,28 @@ exit_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide the info box
     quiz_box.classList.add("activeQuiz"); // show the quiz box
-    showQuestions(3);
+    showQuestions(0);
 }
 let que_count = 0;
+
+const next_btn = quiz_box.querySelector(".next-btn");
+
+//if Next_btn clicked
+ next_btn.onclick = ()=>{
+    if(que_count < questions.length -1){
+        que_count++;
+        showQuestions(que_count);
+    }
+    else{
+        console.log("Questions completed");
+    }
+ }
 
 //getting questions and option from array
 function showQuestions(index){
     const que_text = document.querySelector(".que-text");
     const option_list = document.querySelector(".option-list");
-    let que_tag = '<span>'+ questions[index].question +'</span>';
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question + '</span>';
     let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
                      +'<div class="option">'+ questions[index].options[1] +'<span></span></div>'
                      + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
