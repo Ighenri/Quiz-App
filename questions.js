1474c84917509f18de86260e8dae25d68594c3fd
henry <igwehenry740@gmail.com>
2022-09-07 23:08:12 +0100

    added question in que.js, quiz box functional in script.js

---
+++ questions.js
@@ -0,0 +1,61 @@
+//creating an array and passing the number, questions, and answer
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text ManLanguage Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "HyperText Preprocessor",
+        options: [
+            "HyperText Preprocessor",
+            "HyperText Programming",
+            "HyperText Preprogramming",
+            "HomeText Preprocessor"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structural Query Language",
+        options: [
+            "Styling Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Langauge",
+            "eXecutable Multiple Language",
+            "eXtra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+];
