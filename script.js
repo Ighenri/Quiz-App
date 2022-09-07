7e156ed843b1b3dff8f2a99b30b161407249c7e9
henry <igwehenry740@gmail.com>
2022-09-07 12:21:04 +0100

    continue btn in script file functional

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const start_btn = document.querySelector(".start-btn button");
 const info_box = document.querySelector(".info-box");
 const exit_btn = info_box.querySelector(".buttons .exit");
 const continue_btn = info_box.querySelector(".buttons .continue");
+const quiz_box = document.querySelector(".quiz-box");
 
 //if start quiz button clicked
 start_btn.onclick = ()=>{
@@ -12,4 +13,11 @@ start_btn.onclick = ()=>{
 //if Exit button clicked
 exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide the info box
+    
+}
+
+//if Continue button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide the info box
+    quiz_box.classList.add("activeQuiz"); // show the quiz box
 }
