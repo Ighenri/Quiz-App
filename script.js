f8b89bc2d98fd35a37d0be5aa9fd8a3a41e95fa8
henry <igwehenry740@gmail.com>
2022-09-07 10:01:06 +0100

    all element declared in script file

---
+++ script.js
@@ -0,0 +1,10 @@
+//getting all the required elements
+const start_btn = document.querySelector(".start-btn button");
+const info_box = document.querySelector(".info-box");
+const exit_btn = info_box.querySelector(".buttons .exit");
+const continue_btn = info_box.querySelector(".buttons .continue");
+
+//if start quiz button clicked
+start_btn.onclick = ()=>{
+    info_box.classList.add("");
+}
