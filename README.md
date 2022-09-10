dc075ac18ce29d91a6c856b64c925cb24676dcf3
henry <igwehenry740@gmail.com>
2022-09-10 01:08:29 +0100

    file updates

---
+++ README.md
@@ -0,0 +1,49 @@
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>s
+    </li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+![](./dev%20quiz.png)
+
+## Developer Quiz Application
+
+This is an application that tests developers of their proper understanding on web development and their respective syntaxes
+
+When no answer is supplied by the player, the correct word is displayed
+
+### Built with
+
+HTML
+CSS
+JAVASCRIPT
+
+### Contact
+Twitter: https://twitter.com/IgweHenr1
+
+LinkedIn : https://linkedin.com/in/henryigwe
+
+Projectlink: https://ighenri.github.io/Quiz-App/
+
+
+### Acknowledgement 
+
+I want to acknowledge [web3bridge](https://twitter.com/Web3Bridge) for hosting this cohort and also to my mentor [Jola](https://twitter.com/jolah99). She has been the best tutor to us. I really appreciate you guys 💝
---
+++ questions.js
@@ -0,0 +1,243 @@
+//creating an array and passing the number, questions, and answer
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text ManLanguage Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "HyperText Preprocessor",
+        options: [
+            "HyperText Preprocessor",
+            "HyperText Programming",
+            "HyperText Preprogramming",
+            "HomeText Preprocessor"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Styling Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXtra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Choose the correct HTML element for the largest heading?",
+        answer: "h1",
+        options: [
+            "h2",
+            "h6",
+            "h3",
+            "h1"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "Which CSS property is used to change the text color of an element?",
+        answer: "color",
+        options: [
+            "text-color",
+            "display-color",
+            "color",
+            "coloring"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "How do you create a function in JavaScript?",
+        answer: "function myfunction()",
+        options: [
+            "function myfunction()",
+            "function = myFunction()",
+            "funtion [myFunction]",
+            "function:myFunction()"
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "What boolean operators does JavaScript support?",
+        answer: "&&, || and !",
+        options: [
+            "&&, || and !!",
+            "Strings and Numbers",
+            "$ * () {} []",
+            "&&, || and !"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "Which symbol is used for comments in Javascript?",
+        answer:  "// and /*  */ ",
+        options: [
+            "// and /*  */ ",
+            "/** **/",
+            "//// and /*/*/",
+            "~/ ~/"
+        ]
+    },
+
+    {
+        numb: 11,
+        question: "What are all the looping structures in JavaScript?",
+        answer:  "Do-while loops, For, While",
+        options: [
+            "Jumping, skipping and forwarding",
+            "Do-while loops, For, While",
+            "Do-now loops, Foryou, WhileI",
+            "Swingers, Bulleans, Spartan"
+        ]
+    },
+
+    {
+        numb: 12,
+        question: "What would be the result of 7 +'7'?",
+        answer:  "77",
+        options: [
+            "14",
+            "35",
+            "57",
+            "77"
+    ]
+    },
+
+    {
+        numb: 13,
+        question: "What would be the result of 7 + 7?",
+        answer:  "14",
+        options: [
+            "14",
+            "35",
+            "57",
+            "77"
+    ]
+    },
+
+    {
+        numb: 14,
+        question: "Which of these is not a css syntax",
+        answer:  "aside",
+        options: [
+            "font-size",
+            "aside",
+            "align-text",
+            "background"
+    ]
+    },
+
+    {
+        numb: 15,
+        question: "Which of these is a JS syntax",
+        answer:  "const",
+        options: [
+            "font-size",
+            "const",
+            "div",
+            "background"
+    ]
+    },
+
+    {
+        numb: 16,
+        question: "Which of these has semantic meaning",
+        answer:  "section",
+        options: [
+            "div",
+            "span",
+            "spam",
+            "section"
+    ]
+    },
+    
+    {
+        numb: 17,
+        question: " What is the correct HTML element for inserting a line break?",
+        answer: "br",
+        options: [
+          "break",
+          "lb",
+          "br",
+          "b",
+    ]
+    },
+
+    {
+        numb: 18,
+        question: "One of these is an example of CSS styling method",
+        answer: "inline-style",
+        options: [
+          "interior-style",
+          "exterior-style",
+          "offline-style",
+          "inline-style",
+    ]
+    },
+
+    {
+        numb: 19,
+        question: "Select the odd one amongst the option",
+        answer: "canva",
+        options: [
+          "bootstrap",
+          "tailwind",
+          "canva",
+          "react.js",
+    ]
+    },
+
+    {
+        numb: 20,
+        question: "One who codes the UX of a web page is called",
+        answer: "Frontend",
+        options: [
+          "Frontend",
+          "Backend",
+          "Fullstack",
+          "devOps",
+    ]
+    },
+    
+
+];
